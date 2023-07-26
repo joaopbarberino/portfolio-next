40a5723f8dea5f53d2caa1c511265627a9a4cb66
João Pedro Barberino <42822732+joaopbarberino@users.noreply.github.com>
2023-07-26 02:41:35 -0300

    Add a select and a filter based on project type on admin

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -12,13 +12,26 @@ interface IFormConfig {
     [key: string]: string[];
 };
 
-const DEFAULT_KEY = 'home';
-const RECORD_KEYS = [
+type TPageKey = 'home' | 'who' | 'skills' | 'projects';
+type TProjectType = {
+    label: 'Game Dev' | 'Web Dev',
+    value: '0' | '1'
+};
+
+const DEFAULT_KEY: TPageKey = 'home';
+const RECORD_KEYS: TPageKey[] = [
     'home',
     'who',
     'skills',
     'projects'
 ];
+
+
+const DEFAULT_PROJECT_TYPE: TProjectType = { label: 'Game Dev', value: '0' };
+const PROJECT_TYPES: TProjectType[] = [
+    { label: 'Game Dev', value: '0' },
+    { label: 'Web Dev', value: '1' }
+]
 
 const FORM_FIELDS: IFormConfig = {
     home: ['text'],
@@ -31,7 +44,8 @@ const Admin = () => {
     const [records, setRecords] = useState<IRecord[]>([]);
     const [dialogOpen, setDialogOpen] = useState(false);
     const [selectedRecord, setSelectedRecord] = useState<IRecord>();
-    const [selectedKey, setSelectedKey] = useState(DEFAULT_KEY);
+    const [selectedKey, setSelectedKey] = useState<TPageKey>(DEFAULT_KEY);
+    const [selectedProjectType, setSelectedProjectType] = useState<TProjectType>(DEFAULT_PROJECT_TYPE);
 
     const loadRecords = useCallback(async () => {
         const response = await getAllRecords(selectedKey);
@@ -67,6 +81,14 @@ const Admin = () => {
         }
     };
 
+    const getRecordsToShow = () => {
+        if (selectedKey === 'projects') {
+            return records.filter(record => record.values.filter(value => value.name === 'type' ? value.en === selectedProjectType.value : false)[0] ?? false);
+        }
+
+        return records;
+    }
+
     useEffect(() => {
         loadRecords();
     }, [selectedKey, loadRecords]);
@@ -93,6 +115,18 @@ const Admin = () => {
             </Stack>
 
             <Stack direction={'row'} className='select'>
+                {
+                    selectedKey === 'projects' &&
+                    <Autocomplete
+                        disablePortal
+                        options={PROJECT_TYPES}
+                        value={selectedProjectType}
+                        onChange={(e, newValue) => setSelectedProjectType(newValue ?? DEFAULT_PROJECT_TYPE)}
+                        renderInput={(params) => <TextField color='secondary' {...params} label='Project Type' />}
+                        color='secondary'
+                        sx={{ mr: 2}}
+                    />
+                }
                 <Autocomplete
                     disablePortal
                     options={RECORD_KEYS}
@@ -105,7 +139,7 @@ const Admin = () => {
 
             <Stack spacing={2}>
                 {
-                    records.map(record =>
+                    getRecordsToShow().map(record =>
                         <Card key={record.id} className='record-card'>
                             <CardContent>
                                 <Stack>
